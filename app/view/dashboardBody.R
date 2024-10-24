6862216b87be8ebe07cc9951c7863f6ddfcc96f1
shettysanjay01 <shettysanjay01@gmail.com>
2024-10-24 22:47:47 +0530

    action button setup

--- app/view/dashboardBody.R
+++ app/view/dashboardBody.R
@@ -1,6 +1,6 @@
-box::use(./incomeModule,
-         ./shopAnalysisModule,
-         ./yieldModule)
+box::use(./setupModule,
+         ./dashboardModule,
+         ./overviewModule)
 
 #' @export
 mainBody <- function(ns) {
@@ -9,20 +9,44 @@ mainBody <- function(ns) {
       tabName = 'profitablity_overveiw',
       shiny::br(),
       shiny::br(),
-      yieldModule$yieldTabUI(ns("tab1"))
+      overviewModule$yieldTabUI(ns("tab1"))
     ),
     bs4Dash::tabItem(
       "portfolio_setup",
       shiny::br(),
       shiny::br(),
-      incomeModule$incomeTabUI(ns("tab2"))
+      setupModule$incomeTabUI(ns("tab2")),
+
+      shiny::fixedRow(
+        shiny::column(
+          width = 6,
+          offset = 3,
+          shinyWidgets::actionBttn(
+            inputId = "submit_portfolio",
+            label = "Calculate Results",
+            style = "material-flat",
+            color = "primary"
+          ) |>
+            htmltools::tagAppendAttributes(style = "width: inherit;")
+        ))
+
     ),
 
     bs4Dash::tabItem(
       "dashboard",
       shiny::br(),
       shiny::br(),
-      shopAnalysisModule$shopAnalysisUI(ns("tab3"))
+      dashboardModule$shopAnalysisUI(ns("tab3"))
     )
   )
+}
+
+#' @export
+mainBodyServer <- function(id) {
+  shiny::moduleServer(id,  function(input, output, server){
+    shiny::observeEvent(input$submit_portfolio, {
+      bs4Dash::updateTabItems(session = server, inputId = "sidebar-tabs",
+                               selected = "dashboard")
+    })
+  })
 }
